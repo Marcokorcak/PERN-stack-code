49f045861808eaaebd20ee9d9e95bdb1b88c28f0
Marco Korcak <marcokorcak02@gmail.com>
2022-01-17 21:41:58 -0500

    working on it

--- server/index.js
+++ server/index.js
@@ -1,9 +1,50 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 //middleware
 app.use(cors())
 app.use(express.json());
+
+//routes
+ 
+
+///create a todo
+app.post("/todos", async(req,res) => {
+try{
+const{description} = req.body;
+const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) Returning *", [description]);
+res.json(newTodo.rows[0])
+}catch(err){
+        console.error(err.message)
+    }
+
+});
+
+
+///get all todos
+
+app.get("/todos", async (req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res,json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+
+///get a todo
+app.get("/todos/:id", async(req, res) => {
+    try {
+const {id} = req.params;
+const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+res.json(todo.rows[0]);
+
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 app.listen(5000, () => { console.log("server has started on port 5000")});
