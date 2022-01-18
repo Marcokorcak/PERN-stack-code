bf7e260aec24ebaa053ce7794c9493374843109f
Marco Korcak <marcokorcak02@gmail.com>
2022-01-17 22:25:16 -0500

    client side

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React, {Fragment} from React
+
+
+//components
+
+import InputTodo from './components/InputTodo';
+
+function App() {
+  return (
+    <Fragment>
+      <div className = "container">
+      <InputTodo />
+      </div>
+       </Fragment>
+  );
+}
+
+export default App;
